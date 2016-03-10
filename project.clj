8e15353ba7a6cc67c96b91dcbc6937f3b903b6cb
Karl Mikkelsen <karl@karlmikko.com>
2016-03-10 11:00:56 +1100

    Setup for release to clojars

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject lovii-schema "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject lovii-schema "0.1.1"
+  :description "Describe your application schema using data."
+  :url "https://github.com/LockedOn/lovii-schema"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
   								  [cheshire "5.5.0"]
 				  				  [com.datomic/datomic-free "0.9.5201" :exclusions [joda-time]]]
   				   :plugins [[com.jakemccrary/lein-test-refresh "0.10.0"]]
-				   :main lovii-schema.core}})
+				   :main lovii-schema.core}}
+  :deploy-repositories [["releases" :clojars]])

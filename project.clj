4cdf0a3fa1d5944438ae5b2339d9ba6e10191672
Gardner Vickers <gardner@vickers.me>
2016-03-21 23:07:58 -0400

    Aero reder condtional keypath walking

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
-  								  [cheshire "5.5.0"]
-				  				  [com.datomic/datomic-free "0.9.5201" :exclusions [joda-time]]]
-  				   :plugins [[com.jakemccrary/lein-test-refresh "0.10.0"]]
-				   :main lovii-schema.core}}
+                                  [cheshire "5.5.0"]
+                                  [com.datomic/datomic-free "0.9.5201" :exclusions [joda-time]]
+                                  [aero "0.2.0"]]
+                   :plugins [[com.jakemccrary/lein-test-refresh "0.10.0"]]
+                   :main lovii-schema.core}}
   :deploy-repositories [["releases" :clojars]])
--- src/lovii_schema/util.clj
+++ src/lovii_schema/util.clj
@@ -1,24 +1,42 @@
-(ns lovii-schema.util)
+(ns lovii-schema.util
+  (:require [aero.core :refer [reader read-config]]
+            [clojure.java.io :as io]
+            [clojure.walk :refer [postwalk]]))
 
 (defn- all-variants
-	[schema]
-	(reduce (fn [res m]
-				(conj res (:schema/variant m)))
-			[] schema))
+  [schema]
+  (reduce (fn [res m]
+            (conj res (:schema/variant m)))
+          [] schema))
 
 (def flatten-schema
-	(memoize 
-		(fn [schema]
-			(->> schema
-				 (reduce (fn [res m] 
-							 (-> m
-								(dissoc :schema/variant)
-								(dissoc :schema/abstract)
-								(->> (merge res))))
-						 {})
-				 (merge {:schema/variant {:type :enum 
-				 						  :cardinality :one
-										  :values (reduce (fn [res ke]
-										 					(assoc res ke (str ke))) 
-										 				{} 
-										 				(all-variants schema))}})))))
+  (memoize
+   (fn [schema]
+     (->> schema
+          (reduce (fn [res m]
+                    (-> m
+                        (dissoc :schema/variant)
+                        (dissoc :schema/abstract)
+                        (->> (merge res))))
+                  {})
+          (merge {:schema/variant {:type :enum
+                                   :cardinality :one
+                                   :values (reduce (fn [res ke]
+                                                     (assoc res ke (str ke)))
+                                                   {}
+                                                   (all-variants schema))}})))))
+
+
+;;;
+;;; Here's how to extend the reader
+
+(defmethod reader 'path
+  [_ tag value]
+  (with-meta value {:keypath true}))
+
+
+(let [config (read-config (io/resource "config.edn"))]
+  (postwalk (fn [z] (if (:keypath (meta z))
+                      (get-in config z)
+                      z))
+            config))

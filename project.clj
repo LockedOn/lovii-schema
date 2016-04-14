301ca8970a05ecdf5827326004f2ef1834d000a8
Gardner Vickers <gardner@vickers.me>
2016-04-13 21:23:54 -0700

    Removed aero dependency

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject lockedon/lovii-schema "0.2.1"
+(defproject lockedon/lovii-schema "0.2.2"
   :description "Describe your application schema using data."
   :url "https://github.com/LockedOn/lovii-schema"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [cheshire "5.5.0"]
-                                  [com.datomic/datomic-free "0.9.5201" :exclusions [joda-time]]
-                                  [aero "0.2.0"]]
+                                  [com.datomic/datomic-free "0.9.5201" :exclusions [joda-time]]]
                    :plugins [[com.jakemccrary/lein-test-refresh "0.10.0"]]
                    :main lovii-schema.core}}
   :deploy-repositories [["releases" :clojars]])

f9b48be4ed33050e013cc790997cb28959636238
Karl Mikkelsen <karl@karlmikko.com>
2016-10-17 09:15:42 +1100

    bump dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/LockedOn/lovii-schema"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha13"]
                                   [cheshire "5.5.0"]
-                                  [prismatic/schema "1.0.5"]
+                                  [prismatic/schema "1.1.3"]
                                   [com.datomic/datomic-free "0.9.5201" :exclusions [joda-time]]]
                    :plugins [[com.jakemccrary/lein-test-refresh "0.10.0"]]
                    :main lovii-schema.core}}

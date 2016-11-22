a7e713f7dc262473ad521ceb375912d0c50124fd
Karl Mikkelsen <karl@karlmikko.com>
2016-11-22 13:42:03 +1100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lockedon/lovii-schema "0.2.15"
+(defproject lockedon/lovii-schema "0.2.16"
   :description "Describe your application schema using data."
   :url "https://github.com/LockedOn/lovii-schema"
   :license {:name "MIT"

0793c5ab1e74d7b4169e389b7d3dbfdf8c267619
Karl Mikkelsen <karl@karlmikko.com>
2017-03-03 00:51:36 +1100

    Version 0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lockedon/lovii-schema "0.3.0"
+(defproject lockedon/lovii-schema "0.3.1"
   :description "Describe your application schema using data."
   :url "https://github.com/LockedOn/lovii-schema"
   :license {:name "MIT"
